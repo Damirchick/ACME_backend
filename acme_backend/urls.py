faa19d2b1c8764a41891eeb1be0b0a97ff86c031
DanMagor <anybis-maat@mail.ru>
2018-10-16 19:57:42 +0300

    Template Swagger completed

--- acme_backend/urls.py
+++ acme_backend/urls.py
@@ -17,7 +17,16 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
+
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls'))
+    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^swagger', schema_view)
+
 ]
