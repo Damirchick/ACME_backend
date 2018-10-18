d794343c2cdd40a7dd5cbc91e80f66252aa5d77b
DanMagor <anybis-maat@mail.ru>
2018-10-18 12:01:04 +0300

    swagger test some success

--- acme_backend/urls.py
+++ acme_backend/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='API Documentation', url=r'^api')
 
 
 
